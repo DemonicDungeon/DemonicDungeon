ffe184e6468af61b9f271779bcc7bfd46dca8256
Jesper Tingvall <jesper@tingvall.pw>
2016-01-30 00:04:03 +0100

    Add selectable bottle

---
+++ Assets/PlayerSelect.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSelect : MonoBehaviour {
+
+    private Camera myCamera;
+
+    public float range;
+
+    private SelectableObject LastSelected;
+
+    // Use this for initialization
+    void Start () {
+        myCamera = GetComponent<Camera>();
+	}
+
+    public void Unselect() {
+        if (LastSelected) {
+            LastSelected.Unselect();
+            LastSelected = null;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = myCamera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, range)) {
+
+            SelectableObject selected = hit.collider.GetComponent<SelectableObject>();
+            if (selected != null && selected != LastSelected) {
+                Unselect();
+                selected.Select();
+                LastSelected = selected;
+            }
+
+        } else {
+            Unselect();
+        }
+
+        if (Input.GetButtonDown("Fire1")) {
+            if (LastSelected) {
+                LastSelected.SendMessage("Interact");
+            }
+        }
+    }
+}
