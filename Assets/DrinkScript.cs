38a7b8815782e7ee2b88961f3b3f5c87622e5336
Jesper Tingvall <jesper@tingvall.pw>
2016-01-30 14:46:38 +0100

    Add beer bottle and meta files.

---
+++ Assets/DrinkScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrinkScript : MonoBehaviour {
+
+    public GameState.Drink KindOfDrink;
+
+	// Use this for initialization
+	void Start () {
+        if (KindOfDrink != GameState.HadDrink)
+            gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
