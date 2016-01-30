feb6b37607b011a734ba7ca2beb8e86eae46ba1c
Jesper Tingvall <jesper@tingvall.pw>
2016-01-30 16:32:46 +0100

    Improve fade in animation and add window light

--- Assets/Puzzles/Bedroom/WindowScript.cs
+++ Assets/Puzzles/Bedroom/WindowScript.cs
@@ -6,14 +6,17 @@ public class WindowScript : MonoBehaviour {
     public Material day;
     public Material night;
 
+    public GameObject light;
+
     // Use this for initialization
     void Start () {
         Renderer renderer = GetComponent<Renderer>();
 	    if (GameState.Time == GameState.TimeOfDay.Day) {
             renderer.material = day;
+
         } else {
             renderer.material = night;
-
+            light.SetActive(false);
         }
     }
 	
