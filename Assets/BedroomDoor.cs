28b96baf5a4385313f475c8a1c3986542ee91f97
Jesper Tingvall <jesper@tingvall.pw>
2016-01-30 14:30:49 +0100

    Add bedroom puzzle

---
+++ Assets/BedroomDoor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BedroomDoor : MonoBehaviour {
+
+    private DialogSystem dialog;
+
+    private RoomScript room;
+
+    private bool hasMentionedDrinking;
+
+	// Use this for initialization
+	void Start () {
+        dialog = FindObjectOfType<DialogSystem>();
+        room = FindObjectOfType<RoomScript>();
+	}
+	
+    public void Interact() {
+        if (hasMentionedDrinking)
+            room.GoToNextLevel();
+        else {
+            hasMentionedDrinking = true;
+            dialog.ShowText("Wait. Do I forget something ?");
+        }
+    }
+}
