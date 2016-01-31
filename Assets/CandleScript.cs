23bdfcb73b7d5cc33260b4af1fe8eaa0d48f94ff
Jesper Tingvall <jesper@tingvall.pw>
2016-01-31 14:17:14 +0100

    Add animations to ghost and add candle to coridoor.

---
+++ Assets/CandleScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandleScript : MonoBehaviour {
+
+    public GameState.Candles CandleState;
+
+    // Use this for initialization
+    void Start() {
+        if (CandleState != GameState.CandleLit)
+            gameObject.SetActive(false);
+    }
+}
